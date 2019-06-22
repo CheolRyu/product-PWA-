30e0a708e316bedda908d021c5155a5c6f506870
charlesworld1994@gmail.com <charlesryu@Charless-MacBook-Pro.local>
2019-06-22 08:13:00 -0700

    done!

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-const Button = () => (
-    <TouchableOpacity>
-      <Text>BUY NOW</Text>
+const Button = ({ children }) => {
+  const { buttonStyle, textStyle } = styles;
+  return (
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>{children}</Text>
     </TouchableOpacity>
   );
+};
 
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+};
 export default Button;
