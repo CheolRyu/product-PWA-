13f5e8394637800c510c0646ae6ae6780f2e3d29
charlesworld1994@gmail.com <charlesryu@Charless-MacBook-Pro.local>
2019-06-21 21:42:44 -0700

    initatial app settings

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = props => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: 'rgba(41, 128, 185,1.0)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    paddingTop: 25,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 22,
+    color: '#fff'
+  }
+};
+
+export default Header;
